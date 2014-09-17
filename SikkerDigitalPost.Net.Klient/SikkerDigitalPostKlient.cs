85cce422a420f4b4bd065604a2dbe4bbf0ad408e
jgylta <jgylta@gmail.com>
2014-09-17 14:43:51 +0200

    Lagt til klient og konfigurasjonskode

---
+++ SikkerDigitalPost.Net.Klient/SikkerDigitalPostKlient.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SikkerDigitalPost.Net.Domene;
+using SikkerDigitalPost.Net.Domene.Entiteter;
+
+namespace SikkerDigitalPost.Net.Klient
+{
+    public class SikkerDigitalPostKlient
+    {        
+        /// <param name="tekniskAvsender">
+        /// Teknisk avsender er den parten som har ansvarlig for den tekniske utførelsen av sendingen.
+        /// Teknisk avsender er den aktøren som står for utførelsen av den tekniske sendingen. 
+        /// Hvis sendingen utføres av en databehandler vil dette være databehandleren. 
+        /// Hvis sendingen utføres av behandlingsansvarlige selv er dette den behandlingsansvarlige.
+        /// </param>
+        /// <remarks>
+        /// Se <a href="http://begrep.difi.no/SikkerDigitalPost/forretningslag/Aktorer">oversikt over aktører</a>
+        /// </remarks>
+        public SikkerDigitalPostKlient(TekniskAvsender tekniskAvsender, KlientKonfigurasjon konfigurasjon)
+        {
+            
+            
+        }
+
+        /// <summary>
+        /// Sender en forsendelse til meldingsformidler. Dersom noe feilet i sendingen til meldingsformidler, vil det kastes en exception.
+        /// </summary>
+        /// <param name="forsendelse">Et objekt som har all informasjon klar til å kunne sendes (mottakerinformasjon, sertifikater, dokumenter mm), enten digitalt eller fyisk.</param>
+        public void Send(Forsendelse forsendelse)
+        {
+
+        }
+
+        /// <summary>
+        /// Forespør kvittering for forsendelser. Kvitteringer blir tilgjengeliggjort etterhvert som de er klare i meldingsformidler.
+        /// Det er ikke mulig å etterspørre kvittering for en spesifikk forsendelse.
+        /// </summary>
+        /// <param name="kvitteringForespørsel"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// <list type="table">
+        /// <listheader><description>Dersom det ikke er tilgjengelige kvitteringer skal det ventes følgende tidsintervaller før en ny forespørsel gjøres</description></listheader>
+        /// <item><term>normal</term><description>Minimum 10 minutter</description></item>
+        /// <item><term>prioritert</term><description>Minimum 1 minutt</description></item>
+        /// </list>
+        /// </remarks>
+        public ForretningsKvittering HentKvittering(KvitteringForespørsel kvitteringForespørsel)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Forespør kvittering for forsendelser med mulighet til å samtidig bekrefte på forrige kvittering for å slippe å kjøre eget kall for bekreft. 
+        /// Kvitteringer blir tilgjengeliggjort etterhvert som de er klare i meldingsformidler. Det er ikke mulig å etterspørre kvittering for en 
+        /// spesifikk forsendelse. 
+        /// </summary>
+        /// <param name="kvitteringForespørsel"></param>
+        /// <param name="forrigeKvittering"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// <list type="table">
+        /// <listheader><description>Dersom det ikke er tilgjengelige kvitteringer skal det ventes følgende tidsintervaller før en ny forespørsel gjøres</description></listheader>
+        /// <item><term>normal</term><description>Minimum 10 minutter</description></item>
+        /// <item><term>prioritert</term><description>Minimum 1 minutt</description></item>
+        /// </list>
+        /// </remarks>
+        public ForretningsKvittering HentKvitteringOgBekreftForrige(KvitteringForespørsel kvitteringForespørsel, ForretningsKvittering forrigeKvittering)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Bekreft mottak av forretningskvittering gjennom <see cref="HentKvittering(KvitteringForespørsel)"/>.
+        /// <list type="bullet">
+        /// <listheader><description><para>Dette legger opp til følgende arbeidsflyt</para></description></listheader>
+        /// <item><description><para><see cref="HentKvittering(KvitteringForespørsel)"/></para></description></item>
+        /// <item><description><para>Gjør intern prosessering av kvitteringen (lagre til database, og så videre)</para></description></item>
+        /// <item><description><para>Bekreft mottak av kvittering</para></description></item>
+        /// </list>
+        /// </summary>
+        /// <param name="forrigeKvittering"></param>
+        /// <remarks>
+        /// <see cref="HentKvittering(KvitteringForespørsel)"/> kommer ikke til å returnere en ny kvittering før mottak av den forrige er bekreftet.
+        /// </remarks>
+        public void Bekreft(ForretningsKvittering forrigeKvittering)
+        {
+            
+        }
+    }
+}
