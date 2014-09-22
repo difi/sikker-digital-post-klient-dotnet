2eee2e45e1fb8b1a6214dfdc0f3201090dd42f56
Aleksander Sjåfjell <github@sjafjel.no>
2014-09-22 09:29:50 +0200

    Ryddet litt i testklasse for navn

--- SikkerDigitalPost.Net.Tester/ArkivTester.cs
+++ SikkerDigitalPost.Net.Tester/ArkivTester.cs
@@ -12,9 +12,6 @@ using SikkerDigitalPost.Net.KlientApi;
 
 namespace SikkerDigitalPost.Net.Tests
 {
-    /// <summary>
-    /// Summary description for UnitTest1
-    /// </summary>
     [TestClass]
     public class ArkivTester
     {
@@ -28,16 +25,16 @@ namespace SikkerDigitalPost.Net.Tests
         private static string _manifestFil = "manifest.xml";
         private static string _signaturFil = "signatur.xml";
 
-       public TestContext TestContext { get; set; }
+        public TestContext TestContext { get; set; }
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext context)
         {
-            _testDataMappe = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)),_testDataMappe);
+            _testDataMappe = Path.Combine(path1: Path.GetDirectoryName(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory)), path2: _testDataMappe);
 
-           _vedleggsMappe = Path.Combine(_testDataMappe, _vedleggsMappe);
-           _hoveddokumentMappe = Path.Combine(_testDataMappe, _hoveddokumentMappe);
-            
+            _vedleggsMappe = Path.Combine(_testDataMappe, _vedleggsMappe);
+            _hoveddokumentMappe = Path.Combine(_testDataMappe, _hoveddokumentMappe);
+
             _vedleggsFiler = Directory.GetFiles(_vedleggsMappe);
             _hoveddokument = Directory.GetFiles(_hoveddokumentMappe)[0];
             _signaturFil = Directory.GetFiles(_testDataMappe).Single(f => f.Contains(_signaturFil));
@@ -64,37 +61,42 @@ namespace SikkerDigitalPost.Net.Tests
             var arkiv = new Arkiv(dokumentpakke, new Signatur(_signaturFil), new Manifest(_manifestFil));
 
             var arkivstrøm = arkiv.LagArkiv();
-            
-            using(var zip = new ZipArchive(arkivstrøm, ZipArchiveMode.Read))
+
+            //Åpne zip og generer sjekksum for å verifisere innhold
+            using (var zip = new ZipArchive(arkivstrøm, ZipArchiveMode.Read))
             {
+                
+                //Alle vedlegg
                 foreach (var filsti in _vedleggsFiler)
                 {
-                    byte[] hash1;
-                    byte[] hash2;
+                    byte[] sjekksum1;
+                    byte[] sjekksum2;
 
-                    GenererHasher(zip, filsti, Path.GetFileName(filsti), out hash1, out hash2);
-                    Assert.AreEqual(hash1.ToString(), hash2.ToString());
-                }
-               
-                {
-                    byte[] hash1;
-                    byte[] hash2;
-
-                    GenererHasher(zip, _signaturFil, arkiv.Signatur.Filsti, out hash1, out hash2);
-                    Assert.AreEqual(hash1.ToString(), hash2.ToString());
+                    GenererSjekksum(zip, filsti, Path.GetFileName(filsti), out sjekksum1, out sjekksum2);
+                    Assert.AreEqual(sjekksum1.ToString(), sjekksum2.ToString());
                 }
 
+                //Signaturfil
                 {
-                    byte[] hash1;
-                    byte[] hash2;
+                    byte[] sjekksum1;
+                    byte[] sjekksum2;
 
-                    GenererHasher(zip,_manifestFil, Path.GetFileName(arkiv.Manifest.Filsti), out hash1, out hash2);
-                    Assert.AreEqual(hash1.ToString(), hash2.ToString());
+                    GenererSjekksum(zip, _signaturFil, arkiv.Signatur.Filsti, out sjekksum1, out sjekksum2);
+                    Assert.AreEqual(sjekksum1.ToString(), sjekksum2.ToString());
+                }
+
+                //Manifest
+                {
+                    byte[] sjekksum1;
+                    byte[] sjekksum2;
+
+                    GenererSjekksum(zip, _manifestFil, Path.GetFileName(arkiv.Manifest.Filsti), out sjekksum1, out sjekksum2);
+                    Assert.AreEqual(sjekksum1.ToString(), sjekksum2.ToString());
                 }
             }
         }
 
-        private void GenererHasher(ZipArchive zip, string filstiPåDisk, string entryNavnIArkiv, out byte[] hash1, out byte[] hash2)
+        private void GenererSjekksum(ZipArchive zip, string filstiPåDisk, string entryNavnIArkiv, out byte[] hash1, out byte[] hash2)
         {
             using (var md5 = MD5.Create())
             {
@@ -112,7 +114,7 @@ namespace SikkerDigitalPost.Net.Tests
 
         private Dokument GenererHoveddokument()
         {
-            return new Dokument(Path.GetFileName(_hoveddokument), _hoveddokument,"text/xml");
+            return new Dokument(Path.GetFileName(_hoveddokument), _hoveddokument, "text/xml");
         }
 
         private List<Dokument> GenererVedlegg()
