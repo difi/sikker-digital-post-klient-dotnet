7f190dfaac7f2d6a1cf2722c5a525b836ecd5498
Aleksander Sjåfjell <github@sjafjel.no>
2014-09-29 10:59:01 +0200

    Lagd til enkel boilerplate i Envelope og Header.cs

--- SikkerDigitalPost.Net.KlientApi/Envelope/EnvelopeHeader/Header.cs
+++ SikkerDigitalPost.Net.KlientApi/Envelope/EnvelopeHeader/Header.cs
@@ -1,6 +1,29 @@
-﻿namespace SikkerDigitalPost.Net.KlientApi.Envelope
+﻿using System.Xml;
+
+namespace SikkerDigitalPost.Net.KlientApi.Envelope.EnvelopeHeader
 {
     public class Header
     {
+        private readonly XmlElement _dokument;
+
+        public Header(XmlElement dokument)
+        {
+            _dokument = dokument;
+        }
+
+        public XmlElement Xml()
+        {
+            return null;
+        }
+
+        public XmlElement SecurityElement()
+        {
+            return null;
+        }
+
+        public XmlElement MessagingElement()
+        {
+            return null;
+        }
     }
 }
