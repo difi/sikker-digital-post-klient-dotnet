36f53bf45e808c624b44840832b7364a1d314269
Marius Løkketangen <marius.lokketangen@bekk.no>
2014-09-29 16:21:58 +0200

    Lagt til (tomme) metoder som skal brukes til å generere ulike xml-elementer.

--- SikkerDigitalPost.Net.KlientApi/Envelope/EnvelopeBody/StandardBusinessDocumentHeader.cs
+++ SikkerDigitalPost.Net.KlientApi/Envelope/EnvelopeBody/StandardBusinessDocumentHeader.cs
@@ -1,6 +1,24 @@
-﻿namespace SikkerDigitalPost.Net.KlientApi.Envelope.EnvelopeBody
+﻿using System.Xml;
+
+namespace SikkerDigitalPost.Net.KlientApi.Envelope.EnvelopeBody
 {
-    class StandardBusinessDocumentHeader
+    public class StandardBusinessDocumentHeader
     {
+        private readonly XmlDocument _dokument;
+
+        public StandardBusinessDocumentHeader(XmlDocument dokument)
+        {
+            _dokument = dokument;
+        }
+
+        public XmlElement Xml()
+        {
+            var sbdHeaderElement = _dokument.CreateElement("ns3", "StandardBusinessDocumentHeader", _dokument.NamespaceURI);
+            var headerVersion = _dokument.CreateElement("ns3", "StandarBusinessDocumentHeader", _dokument.NamespaceURI);
+            headerVersion.InnerText = "1.0";
+            sbdHeaderElement.AppendChild(headerVersion);
+
+            return sbdHeaderElement;
+        }
     }
 }
