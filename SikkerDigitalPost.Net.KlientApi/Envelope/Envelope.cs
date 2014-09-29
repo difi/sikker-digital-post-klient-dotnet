43ff5d04754c37eaa1a1b595683ed92eec2e6513
Aleksander Sjåfjell <github@sjafjel.no>
2014-09-29 10:14:07 +0200

    Struktur for oppbygging av dokument er satt i Envelope-klassen.

--- SikkerDigitalPost.Net.KlientApi/Envelope/Envelope.cs
+++ SikkerDigitalPost.Net.KlientApi/Envelope/Envelope.cs
@@ -1,6 +1,37 @@
-﻿namespace SikkerDigitalPost.Net.KlientApi.Envelope
+﻿using System.Xml;
+using SikkerDigitalPost.Net.Domene.Entiteter;
+
+namespace SikkerDigitalPost.Net.KlientApi.Envelope
 {
     public class Envelope
     {
+        public const string NsXlmnsEnv = "http://www.w3.org/2003/05/soap-envelope";
+
+        public Arkiv arkiv { get; set; }
+        
+        private XmlDocument _envelopeXml;
+        
+        public Envelope()
+        {
+            _envelopeXml = EnvelopeDokument();
+        }
+
+        private XmlDocument EnvelopeDokument()
+        {
+            var xmlDokument = new XmlDocument();
+            return xmlDokument;
+        }
+
+        private XmlElement HeaderElement()
+        {
+            //var header = new Header(_envelopeXml.CreateElement("header", "namespace", "..."));
+            //_envelopeXml.DocumentElement.AppendChild(header.Element);
+            return null;
+        }
+
+        private XmlElement BodyElement()
+        {
+            return null;
+        }
     }
 }
