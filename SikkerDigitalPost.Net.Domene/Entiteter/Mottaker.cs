75a58dc88575366d271483778e0ad51dbfbd1bf0
Aleksander Sjåfjell <github@sjafjel.no>
2014-09-29 15:43:36 +0200

    Lagd mer detaljert beskrivelse av prosjektet i program.cs.

--- SikkerDigitalPost.Net.Domene/Entiteter/Mottaker.cs
+++ SikkerDigitalPost.Net.Domene/Entiteter/Mottaker.cs
@@ -5,7 +5,7 @@ namespace SikkerDigitalPost.Net.Domene.Entiteter
     public class Mottaker : Person
     {
         public X509Certificate2 MottakerSerfifikat { get; set; }
-        public string OrganisasjonsnummerPostkasse { get; set; }
+        public Organisasjonsnummer OrganisasjonsnummerPostkasse { get; set; }
 
         /// <summary>
         /// Informasjon om mottaker. Vil vanligvis være hentet fra http://begrep.difi.no/Oppslagstjenesten/.
@@ -14,10 +14,23 @@ namespace SikkerDigitalPost.Net.Domene.Entiteter
         /// <param name="postkasseadresse">Mottakerens adresse hos postkasseleverandøren.</param>
         /// <param name="mottakerSerfifikat">Mottakerens sertifikat.</param>
         /// <param name="organisasjonsnummerPostkasse">Identifikator (organisasjonsnummer) til virksomheten som er sluttmottaker i meldingsprosessen.</param>
-        public Mottaker(string personidentifikator, string postkasseadresse, X509Certificate2 mottakerSerfifikat, string organisasjonsnummerPostkasse) : base(personidentifikator, postkasseadresse)
+        public Mottaker(string personidentifikator, string postkasseadresse, X509Certificate2 mottakerSerfifikat, Organisasjonsnummer organisasjonsnummerPostkasse) : base(personidentifikator, postkasseadresse)
         {
             MottakerSerfifikat = mottakerSerfifikat;
             OrganisasjonsnummerPostkasse = organisasjonsnummerPostkasse;
+        }
+
+
+        /// <summary>
+        /// Informasjon om mottaker. Vil vanligvis være hentet fra http://begrep.difi.no/Oppslagstjenesten/.
+        /// </summary>
+        /// <param name="personidentifikator">Identifikator (fødselsnummer eller D-nummer) til mottaker av brevet.</param>
+        /// <param name="postkasseadresse">Mottakerens adresse hos postkasseleverandøren.</param>
+        /// <param name="mottakerSerfifikat">Mottakerens sertifikat.</param>
+        /// <param name="organisasjonsnummerPostkasse">Identifikator (organisasjonsnummer) til virksomheten som er sluttmottaker i meldingsprosessen.</param>
+        public Mottaker(string personidentifikator, string postkasseadresse, X509Certificate2 mottakerSerfifikat, string organisasjonsnummerPostkasse)
+            : this(personidentifikator,postkasseadresse,mottakerSerfifikat,new Organisasjonsnummer(organisasjonsnummerPostkasse))
+        {
         }
     }
 }
