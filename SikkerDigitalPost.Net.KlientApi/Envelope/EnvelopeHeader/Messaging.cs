40e3e5e9cbd76d24fc52cde959c4ca0a215dafe1
Aleksander Sjåfjell <github@sjafjel.no>
2014-09-30 10:57:02 +0200

    Lagt til Messaging med litt boilerplate, og lagd XmlPart-klasse som får bort litt boilerplate

--- SikkerDigitalPost.Net.KlientApi/Envelope/EnvelopeHeader/Messaging.cs
+++ SikkerDigitalPost.Net.KlientApi/Envelope/EnvelopeHeader/Messaging.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Xml;
 
-namespace SikkerDigitalPost.Net.KlientApi.Envelope
+namespace SikkerDigitalPost.Net.KlientApi.Envelope.EnvelopeHeader
 {
-    class Messaging
+    public class Messaging : XmlPart
     {
+        public Messaging(XmlDocument xmlDocument) : base(xmlDocument)
+        {
+        }
+
+        public override XmlElement Xml()
+        {
+            return null;
+        }
+
+        public XmlElement UserMessage()
+        {
+            return null;
+        }
+
+        public XmlElement MessageInfo()
+        {
+            return null;
+        }
+
+        public XmlElement PartyInfo()
+        {
+            //From 
+
+
+            //To
+            return null;
+        }
+
+        public XmlElement CollaborationInfo()
+        {
+            //AgreementRef
+
+            //Service
+
+            //Action
+
+            //ConversationId
+            return null;
+        }
+
+        public XmlElement PayloadInfo()
+        {
+            return null;
+        }
     }
 }
--- SikkerDigitalPost.Net.KlientApi/Envelope/EnvelopeHeader/Security.cs
+++ SikkerDigitalPost.Net.KlientApi/Envelope/EnvelopeHeader/Security.cs
@@ -5,6 +5,9 @@ namespace SikkerDigitalPost.Net.KlientApi.Envelope.EnvelopeHeader
     class Security
     {
         private readonly XmlDocument _dokument;
+        
+        private readonly string wsse ="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd";
+        private readonly string wsu = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd";
 
         public Security(XmlDocument dokument)
         {
@@ -13,7 +16,17 @@ namespace SikkerDigitalPost.Net.KlientApi.Envelope.EnvelopeHeader
 
         public XmlElement Xml()
         {
-            return null;
+            var securityElement = _dokument.CreateElement("wsse", "Security", wsse);
+            securityElement.SetAttribute("xmlns:wsu", wsu);
+            securityElement.AppendChild(BinarySecurityToken());
+            return securityElement;
+        }
+
+        public XmlElement BinarySecurityToken()
+        {
+            var binarySecurityToken = _dokument.CreateElement("wsse", "BinarySecurityToken");
+            binarySecurityToken.SetAttribute("xmlns", "wsse", wsse);
+            return binarySecurityToken;
         }
     }
 }
