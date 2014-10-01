2070bbcc7e659f94128ad44b8c1e7b7a24cda6ec
jgylta <jgylta@gmail.com>
2014-10-01 15:18:14 +0200

    * Fikset funksjonalitet for å legge til Qualifying Properties Object i dokumenter uten namespace prefixer * Rensket litt opp i interface bruk * Lagt til funksjonalitet for verifisering av signatur når SecurityTokenReference brukes

--- SikkerDigitalPost.Net.KlientApi/Xml/SignedXmlWithAgnosticId.cs
+++ SikkerDigitalPost.Net.KlientApi/Xml/SignedXmlWithAgnosticId.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography.Xml;
 using System.Xml;
@@ -13,12 +14,17 @@ namespace SikkerDigitalPost.Net.KlientApi.Xml
     /// </remarks>
     internal sealed class SignedXmlWithAgnosticId : SignedXml
     {
-        public SignedXmlWithAgnosticId(XmlDocument xml): base(xml)
+        private XmlDocument m_containingDocument;
+
+        public SignedXmlWithAgnosticId(XmlDocument xml)
+            : base(xml)
         {
+            this.m_containingDocument = xml;
         }
 
         public SignedXmlWithAgnosticId(XmlElement xmlElement): base(xmlElement)
         {
+            this.m_containingDocument = xmlElement.OwnerDocument;
         }
 
         /// <summary>
@@ -29,7 +35,7 @@ namespace SikkerDigitalPost.Net.KlientApi.Xml
         /// <param name="inclusiveNamespacesPrefixList">An optional list of namespaces to be set as the canonicalization namespace prefix list.</param>
         public SignedXmlWithAgnosticId(XmlDocument xml, X509Certificate2 certificate, string inclusiveNamespacesPrefixList = null): base(xml)
         {
-            Initialize(certificate, inclusiveNamespacesPrefixList);
+            Initialize(xml, certificate, inclusiveNamespacesPrefixList);
         }
 
         /// <summary>
@@ -41,10 +47,10 @@ namespace SikkerDigitalPost.Net.KlientApi.Xml
         public SignedXmlWithAgnosticId(XmlElement xmlElement, X509Certificate2 certificate, string inclusiveNamespacesPrefixList = null)
             : base(xmlElement)
         {
-            Initialize(certificate, inclusiveNamespacesPrefixList);
+            Initialize(xmlElement.OwnerDocument, certificate, inclusiveNamespacesPrefixList);
         }
 
-        private void Initialize(X509Certificate2 certificate, string inclusiveNamespacesPrefixList = null)
+        private void Initialize(XmlDocument xml, X509Certificate2 certificate, string inclusiveNamespacesPrefixList = null)
         {
             const string signatureMethod = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
 
@@ -60,6 +66,8 @@ namespace SikkerDigitalPost.Net.KlientApi.Xml
             SignedInfo.CanonicalizationMethod = "http://www.w3.org/2001/10/xml-exc-c14n#";
             if (inclusiveNamespacesPrefixList != null)
                 ((XmlDsigExcC14NTransform)SignedInfo.CanonicalizationMethodObject).InclusiveNamespacesPrefixList = inclusiveNamespacesPrefixList;
+
+            this.m_containingDocument = xml;
         }
 
         public override XmlElement GetIdElement(XmlDocument doc, string id)
@@ -100,6 +108,36 @@ namespace SikkerDigitalPost.Net.KlientApi.Xml
                 result = node.SelectSingleNode(string.Format("//*[@*[local-name() = '{0}'] = '{1}']", s, idValue)) as XmlElement;
                 if (result != null)
                     break;
+            }
+
+            return result;
+        }
+
+        protected override AsymmetricAlgorithm GetPublicKey()
+        {
+            AsymmetricAlgorithm result = base.GetPublicKey();
+
+            // If we have no key, attemt to find one via a SecurityTokenReference.
+            if (result == null && this.KeyInfo != null)
+            {
+                var keyinfo = this.KeyInfo.GetXml();
+                if (keyinfo != null)
+                {
+                    XmlNamespaceManager mgr = new XmlNamespaceManager(keyinfo.OwnerDocument.NameTable);
+                    mgr.AddNamespace("wsse", "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd");
+                    var reference = keyinfo.SelectSingleNode("./wsse:SecurityTokenReference/wsse:Reference", mgr);
+                    if (reference != null)
+                    {
+                        var targetId = reference.Attributes["URI"].Value;
+                        if (targetId.StartsWith("#"))
+                            targetId = targetId.Substring(1);
+                        var keyElement = FindIdElement(m_containingDocument, targetId);
+                        if (keyElement != null && !string.IsNullOrEmpty(keyElement.InnerText))
+                        {
+                            result = new X509Certificate2(Convert.FromBase64String(keyElement.InnerText)).PublicKey.Key;
+                        }
+                    }
+                }
             }
 
             return result;
