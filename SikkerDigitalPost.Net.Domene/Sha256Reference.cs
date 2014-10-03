70a07869e6d5d9012f2b9d86cc4c2bfcaa24ca46
Aleksander Sjåfjell <github@sjafjel.no>
2014-10-03 09:36:45 +0200

    gitt riktig navnerom for Sha256Reference

--- SikkerDigitalPost.Net.Domene/Sha256Reference.cs
+++ SikkerDigitalPost.Net.Domene/Sha256Reference.cs
@@ -1,6 +1,6 @@
 ﻿using System.IO;
 
-namespace SikkerDigitalPost.Net.KlientApi.Xml
+namespace SikkerDigitalPost.Net.Domene
 {
     /// <summary>
     /// Represents the &lt;reference&gt; element of an Xml Signature with http://www.w3.org/2001/04/xmlenc#sha256 as the digest method.
