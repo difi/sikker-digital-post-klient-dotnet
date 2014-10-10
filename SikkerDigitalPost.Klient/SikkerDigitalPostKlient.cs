cbff1c7288ae7b4868f4c2b97f167dbbca698554
Marius Løkketangen <marius.lokketangen@bekk.no>
2014-10-10 14:40:41 +0200

    Startet validering av respons etter sending.

--- SikkerDigitalPost.Klient/SikkerDigitalPostKlient.cs
+++ SikkerDigitalPost.Klient/SikkerDigitalPostKlient.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
+using System.Security.Cryptography.Xml;
+using System.Xml;
 using System.Xml.Linq;
 using SikkerDigitalPost.Domene.Entiteter.Aktører;
 using SikkerDigitalPost.Domene.Entiteter.AsicE.Manifest;
@@ -9,6 +13,7 @@ using SikkerDigitalPost.Domene.Entiteter.Kvitteringer;
 using SikkerDigitalPost.Domene.Entiteter.Post;
 using SikkerDigitalPost.Klient.Envelope;
 using SikkerDigitalPost.Klient.Utilities;
+using SikkerDigitalPost.Klient.Xml;
 
 namespace SikkerDigitalPost.Klient
 {
@@ -73,34 +78,55 @@ namespace SikkerDigitalPost.Klient
             //    ? @"Z:\Development\Digipost\Envelope.xml"
             //    : @"C:\Prosjekt\DigiPost\Temp\Envelope.xml");
 
-            var soapContainer = new SoapContainer();
-            soapContainer.Envelope = envelope;
+            var soapContainer = new SoapContainer {Envelope = envelope, Action = "\"\""};
             soapContainer.Vedlegg.Add(arkiv);
-            soapContainer.Action = "\"\"";
 
-            SendSoapContainer(soapContainer);
-             //Lag request
+            var responseXml = SendSoapContainer(soapContainer);
 
-            //var request = (HttpWebRequest)WebRequest.Create("https://qaoffentlig.meldingsformidler.digipost.no/api/ebms");
-            //soapContainer.Send(request);
-            //try
-            //{
-            //    var response = request.GetResponse();
-            //    var data = new StreamReader(response.GetResponseStream()).ReadToEnd();
-            //    //return true;
-            //}
-            //catch (WebException we)
-            //{
-            //    using (var response = we.Response as HttpWebResponse)
-            //    {
-            //        using (Stream errorStream = response.GetResponseStream())
-            //        {
-            //            var soap = XDocument.Load(errorStream);
-            //            throw new Exception("En feil");
-            //        }
+            //Valider signatur
+            XmlNode rot = responseXml.DocumentElement;
+            XmlNamespaceManager responseMgr = new XmlNamespaceManager(responseXml.NameTable);
+            responseMgr.AddNamespace("ds", Navnerom.ds);
+            responseMgr.AddNamespace("ns5", Navnerom.Ns5);
+            responseMgr.AddNamespace("ns7", Navnerom.Ns7);
+            
+            try
+            {
+                XmlNode signatureNode = rot.SelectSingleNode("ds:Signature", responseMgr);
+                //valider
+            }
+            catch (Exception e)
+            {
+                throw new Exception("", e);
+            }
 
-            //    }
-            //}
+            //Valider body og attachment
+            try
+            {
+                //Hent body og attachment digests
+                var bodyReference = rot.SelectSingleNode("//ns5:Reference[@URI = #" + guidHandler.BodyId + "]", responseMgr);
+                var asicReference = rot.SelectSingleNode("//ns5:Reference[@URI = cid:" + guidHandler.DokumentpakkeId + "]", responseMgr);
+                var bodyDigest = bodyReference.SelectSingleNode("ns5:DigestValue", responseMgr).InnerText;
+                var asicDigest = asicReference.SelectSingleNode("ns5:DigestValue", responseMgr).InnerText;
+
+                //Valider mot envelope
+                var envelopeXml = envelope.Xml();
+                var envelopeRoot = envelopeXml.DocumentElement;
+                XmlNamespaceManager envelopeMgr = new XmlNamespaceManager(envelopeXml.NameTable);
+                envelopeMgr.AddNamespace("", "http://www.w3.org/2000/09/xmldsig#");
+                var envBodyReference = rot.SelectSingleNode("Signature/SignedInfo/Reference[@URI = " + guidHandler.BodyId + "]", envelopeMgr);
+                var envAsicReference = rot.SelectSingleNode("Signature/SignedInfo/Reference[@URI = " + guidHandler.DokumentpakkeId + "]", envelopeMgr);
+                var envBodyDigest = envBodyReference.SelectSingleNode("DigestValue", envelopeMgr).InnerText;
+                var envAsicDigest = envAsicReference.SelectSingleNode("DigestValue", envelopeMgr).InnerText;
+                if (!bodyDigest.Equals(envBodyDigest) || !envAsicDigest.Equals(asicDigest))
+                {
+                    throw new Exception("Digests are not equal!");
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Fant ikke...", e);
+            }
         }
 
         /// <summary>
@@ -164,15 +190,21 @@ namespace SikkerDigitalPost.Klient
             
         }
 
-        private void SendSoapContainer(SoapContainer soapContainer)
+        private XmlDocument SendSoapContainer(SoapContainer soapContainer)
         {
             var request = (HttpWebRequest)WebRequest.Create("https://qaoffentlig.meldingsformidler.digipost.no/api/ebms");
             soapContainer.Send(request);
             try
             {
-                var response = request.GetResponse();
-                var data = new StreamReader(response.GetResponseStream()).ReadToEnd();
-                //return true;
+                using (Stream responseStream = request.GetResponse().GetResponseStream())
+                {
+                    using (XmlReader xmlReader = XmlReader.Create(responseStream))
+                    {
+                        var xmlDocument = new XmlDocument();
+                        xmlDocument.Load(xmlReader);
+                        return xmlDocument;
+                    }
+                }
             }
             catch (WebException we)
             {
