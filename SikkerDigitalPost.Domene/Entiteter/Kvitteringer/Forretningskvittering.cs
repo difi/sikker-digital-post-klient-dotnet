4456fd8a31bcb8d455bef27e6718bc76c8b13cef
Marius Løkketangen <marius.lokketangen@bekk.no>
2014-10-14 17:58:18 +0200

    Laget metode for å hente ut referansen til body-elementet i Forretningskvittering

--- SikkerDigitalPost.Domene/Entiteter/Kvitteringer/Forretningskvittering.cs
+++ SikkerDigitalPost.Domene/Entiteter/Kvitteringer/Forretningskvittering.cs
@@ -1,10 +1,47 @@
-﻿namespace SikkerDigitalPost.Domene.Entiteter.Kvitteringer
+﻿using System;
+using System.Xml;
+
+namespace SikkerDigitalPost.Domene.Entiteter.Kvitteringer
 {
-    public class Forretningskvittering : Kvittering
+    public abstract class Forretningskvittering
     {
-        public Forretningskvittering()
+        public DateTime Tidspunkt { get; protected set; }
+
+        public string KonversasjonsId { get; protected set; }
+
+        public string MessageId { get; protected set; }
+
+        public string RefToMessageId { get; protected set; }
+        
+        internal XmlNode BodyReference { get; set; }
+
+        protected static XmlNode DocumentNode(XmlDocument document, XmlNamespaceManager namespaceManager, string xPath)
         {
+            var rot = document.DocumentElement;
+            var targetNode = rot.SelectSingleNode(xPath, namespaceManager);
+
+            return targetNode;
+        }
+
+        protected static XmlNode BodyReferenceNode(XmlDocument document, XmlNamespaceManager namespaceManager)
+        {
+            var rot = document.DocumentElement;
             
+            var partInfo = rot.SelectSingleNode("//ns6:PartInfo", namespaceManager);
+            var partInfoBodyId = "";
+            if (partInfo.Attributes.Count > 0)
+                partInfoBodyId = partInfo.Attributes["href"].Value;
+
+            var bodyId = rot.SelectSingleNode("//env:Body", namespaceManager).Attributes["wsu:Id"].Value;
+
+            if (!partInfoBodyId.Equals(String.Empty) && !bodyId.Equals(partInfoBodyId))
+            {
+                throw new Exception("Id i PartInfo og i Body matcher ikke.");
+            }
+            
+            var bodyReferenceNode = rot.SelectSingleNode("//ns5:Reference[@URI = '#" + bodyId + "']", namespaceManager);
+
+            return bodyReferenceNode;
         }
     }
 }
