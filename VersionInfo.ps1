9fe4ce201daf7ca2057d805238e31f9c42803c97
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2014-10-15 15:10:28 +0200

    Lagt til beskrivelse av nuget-release

--- VersionInfo.ps1
+++ VersionInfo.ps1
@@ -1,7 +1,7 @@
-"AssemblyVersion (SikkerdigitalPost.Net.Domene.dll):"
-[Reflection.AssemblyName]::GetAssemblyName("SikkerDigitalPost.Net.KlientApi\bin\Release\SikkerDigitalPost.Net.Domene.dll").Version.ToString()
+"AssemblyVersion (SikkerdigitalPost.Domene.dll):"
+[Reflection.AssemblyName]::GetAssemblyName("SikkerDigitalPost.Klient\bin\Release\SikkerDigitalPost.Domene.dll").Version.ToString()
 ""
-"AssemblyVersion (SikkerdigitalPost.Net.KlientApi.dll):"
-[Reflection.AssemblyName]::GetAssemblyName("SikkerDigitalPost.Net.KlientApi\bin\Release\SikkerDigitalPost.Net.KlientApi.dll").Version.ToString()
+"AssemblyVersion (SikkerdigitalPost.Klient.dll):"
+[Reflection.AssemblyName]::GetAssemblyName("SikkerDigitalPost.Klient\bin\Release\SikkerDigitalPost.Klient.dll").Version.ToString()
 
 Read-Host "Trykk en tast for å lukke vinduet."
