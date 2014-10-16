69d647f169f8e9c8b63bf24da46b47216be12c3a
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2014-10-16 10:16:02 +0200

    Lagt til SDPException, SendException og KonfigurasjonsException

---
+++ SikkerDigitalPost.Domene/Exceptions/KonfigurasjonsException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SikkerDigitalPost.Domene.Exceptions
+{
+    public class KonfigurasjonsException : SikkerDigitalPostException
+    {
+        public KonfigurasjonsException(){
+            
+        }
+
+        public KonfigurasjonsException(string message) : base(message)
+        {
+            
+        }
+
+        public KonfigurasjonsException(string message, Exception inner) : base (message, inner)
+        {
+            
+        }
+
+
+    }
+}
