7f61921d10c2fbc8ece101c1950011b34a143c3d
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2014-10-21 16:02:01 +0200

    Lagt til eboks

---
+++ SikkerDigitalPost.Testklient/PostkasseInnstillinger.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SikkerDigitalPost.Testklient
+{
+    public class PostkasseInnstillinger
+    {
+        /*
+        * Følgende sertifikater må brukes for å kunne sende digital post
+        * 
+        * - Mottagersertifikat brukes for å kryptere og signere dokumentpakke som skal til mottagerens postkasse.
+        * - TekniskAvsenderSertifikat brukes for sikker kommunikasjon med meldingsformidler.
+        */
+        public X509Certificate2 Avsendersertifikat { get; set; }
+        public X509Certificate2 Mottakersertifikat { get; set; }
+
+        public string OrgNummerBehandlingsansvarlig { get; set; }
+        public string OrgNummerDatabehandler { get; set; }
+        public string OrgnummerPostkasse { get; set; }
+        public string Personnummer { get; set; }
+        public string Postkasseadresse { get; set; }
+
+        private PostkasseInnstillinger(X509Certificate2 avsendersertifikat, X509Certificate2 mottakersertifikat, string orgNummerBehandlingsansvarlig, string orgNummerDatabehandler, string orgnummerPostkasse,
+            string personnummer, string postkasseadresse)
+        {
+            Avsendersertifikat = avsendersertifikat;
+            Mottakersertifikat = mottakersertifikat;
+            OrgNummerBehandlingsansvarlig = orgNummerBehandlingsansvarlig;
+            OrgNummerDatabehandler = orgNummerDatabehandler;
+            OrgnummerPostkasse = orgnummerPostkasse;
+            Personnummer = personnummer;
+            Postkasseadresse = postkasseadresse;
+        }
+
+        public static PostkasseInnstillinger GetEboks()
+        {
+            X509Certificate2 tekniskAvsenderSertifikat;
+            {
+                X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                store.Open(OpenFlags.ReadOnly);
+                tekniskAvsenderSertifikat = store.Certificates.Find(
+                    X509FindType.FindByThumbprint, "8702F5E55217EC88CF2CCBADAC290BB4312594AC", true)[0];
+                store.Close();
+            }
+
+            X509Certificate2 mottakerSertifikat;
+            {
+                X509Store store2 = new X509Store(StoreName.TrustedPeople, StoreLocation.CurrentUser);
+                store2.Open(OpenFlags.ReadOnly);
+                mottakerSertifikat =
+                    store2.Certificates.Find(X509FindType.FindByThumbprint, "7166484C1116D8A32AACD49FD2FB79F06751661D", true)[0];
+                store2.Close();
+            }
+
+            var orgnummerPosten = "984661185";
+            var orgnummerDatabehandler = orgnummerPosten;
+            var orgnummerBehandlingsansvarlig = orgnummerPosten;
+            var orgnummerPostkasse = "996460320";
+            var mottakerPersonnummer = "01043100358";
+            var mottakerPostkasse = "0000485509";
+
+            return new PostkasseInnstillinger(tekniskAvsenderSertifikat, mottakerSertifikat, orgnummerBehandlingsansvarlig, orgnummerDatabehandler, orgnummerPostkasse, mottakerPersonnummer, mottakerPostkasse);
+        }
+
+        public static PostkasseInnstillinger GetPosten()
+        {
+            X509Certificate2 tekniskAvsenderSertifikat;
+            {
+                X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+                store.Open(OpenFlags.ReadOnly);
+                tekniskAvsenderSertifikat = store.Certificates.Find(
+                    X509FindType.FindByThumbprint, "8702F5E55217EC88CF2CCBADAC290BB4312594AC", true)[0];
+                store.Close();
+            }
+
+            X509Certificate2 mottakerSertifikat;
+            {
+                X509Store store2 = new X509Store(StoreName.TrustedPeople, StoreLocation.CurrentUser);
+                store2.Open(OpenFlags.ReadOnly);
+                mottakerSertifikat =
+                    store2.Certificates.Find(X509FindType.FindByThumbprint, "B43CAAA0FBEE6C8DA85B47D1E5B7BCAB42AB9ADD", true)[0];
+                store2.Close();
+            }
+
+            var orgnummerPosten = "984661185";
+            var orgnummerDatabehandler = orgnummerPosten;
+            var orgnummerBehandlingsansvarlig = orgnummerPosten;
+            var orgnummerPostkasse = orgnummerPosten;
+            var mottakerPersonnummer = "04036125433";
+            var mottakerPostkasse = "ove.jonsen#6K5A";
+
+            return new PostkasseInnstillinger(tekniskAvsenderSertifikat, mottakerSertifikat, orgnummerBehandlingsansvarlig, orgnummerDatabehandler, orgnummerPostkasse, mottakerPersonnummer, mottakerPostkasse);
+        }
+    }
+}
--- SikkerDigitalPost.Testklient/Program.cs
+++ SikkerDigitalPost.Testklient/Program.cs
@@ -13,115 +13,22 @@ namespace SikkerDigitalPost.Testklient
 {
     class Program
     {
-
-        private class PostkasseSettings
-        {
-            /*
-            * Følgende sertifikater må brukes for å kunne sende digital post
-            * 
-            * - Mottagersertifikat brukes for å kryptere og signere dokumentpakke som skal til mottagerens postkasse.
-            * - TekniskAvsenderSertifikat brukes for sikker kommunikasjon med meldingsformidler.
-            */
-            public X509Certificate2 Avsendersertifikat { get; set; }
-            public X509Certificate2 Mottakersertifikat { get; set; }
-
-            public string OrgNummerBehandlingsansvarlig { get; set; }
-            public string OrgNummerDatabehandler { get; set; }
-            public string OrgnummerPostkasse { get; set; }
-            public string Personnummer { get; set; }
-            public string Postkasseadresse { get; set; }
-
-            private PostkasseSettings(X509Certificate2 avsendersertifikat, X509Certificate2 mottakersertifikat, string orgNummerBehandlingsansvarlig, string orgNummerDatabehandler, string orgnummerPostkasse,
-                string personnummer, string postkasseadresse)
-            {
-                Avsendersertifikat = avsendersertifikat;
-                Mottakersertifikat = mottakersertifikat;
-                OrgNummerBehandlingsansvarlig = orgNummerBehandlingsansvarlig;
-                OrgNummerDatabehandler = orgNummerDatabehandler;
-                OrgnummerPostkasse = orgnummerPostkasse;
-                Personnummer = personnummer;
-                Postkasseadresse = postkasseadresse;
-            }
-
-            public static PostkasseSettings GetEboks()
-            {
-                X509Certificate2 tekniskAvsenderSertifikat;
-                {
-                    X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
-                    store.Open(OpenFlags.ReadOnly);
-                    tekniskAvsenderSertifikat = store.Certificates.Find(
-                        X509FindType.FindByThumbprint, "8702F5E55217EC88CF2CCBADAC290BB4312594AC", true)[0];
-                    store.Close();
-                }
-
-                X509Certificate2 mottakerSertifikat;
-                {
-                    X509Store store2 = new X509Store(StoreName.TrustedPeople, StoreLocation.CurrentUser);
-                    store2.Open(OpenFlags.ReadOnly);
-                    mottakerSertifikat =
-                        store2.Certificates.Find(X509FindType.FindByThumbprint, "B43CAAA0FBEE6C8DA85B47D1E5B7BCAB42AB9ADD", true)[0];
-                    store2.Close();
-                }
-
-                var orgnummerPosten = "984661185";
-                var orgnummerDatabehandler = orgnummerPosten;
-                var orgnummerBehandlingsansvarlig = orgnummerPosten;
-                var orgnummerPostkasse = "996460320";
-                var mottakerPersonnummer = "01043100358";
-                var mottakerPostkasse = "0000485509";
-
-                return new PostkasseSettings(tekniskAvsenderSertifikat,mottakerSertifikat, orgnummerBehandlingsansvarlig, orgnummerDatabehandler, orgnummerPostkasse, mottakerPersonnummer, mottakerPostkasse);   
-            }
-
-            public static PostkasseSettings GetPosten ()
-            {
-                X509Certificate2 tekniskAvsenderSertifikat;
-                {
-                    X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
-                    store.Open(OpenFlags.ReadOnly);
-                    tekniskAvsenderSertifikat = store.Certificates.Find(
-                        X509FindType.FindByThumbprint, "8702F5E55217EC88CF2CCBADAC290BB4312594AC", true)[0];
-                    store.Close();
-                }
-
-                X509Certificate2 mottakerSertifikat;
-                {
-                    X509Store store2 = new X509Store(StoreName.TrustedPeople, StoreLocation.CurrentUser);
-                    store2.Open(OpenFlags.ReadOnly);
-                    mottakerSertifikat =
-                        store2.Certificates.Find(X509FindType.FindByThumbprint, "B43CAAA0FBEE6C8DA85B47D1E5B7BCAB42AB9ADD", true)[0];
-                    store2.Close();
-                }
-
-                var orgnummerPosten = "984661185";
-                var orgnummerDatabehandler = orgnummerPosten;
-                var orgnummerBehandlingsansvarlig = orgnummerPosten;
-                var orgnummerPostkasse = orgnummerPosten;
-                var mottakerPersonnummer = "04036125433";
-                var mottakerPostkasse = "ove.jonsen#6K5A";
-
-                return new PostkasseSettings(tekniskAvsenderSertifikat, mottakerSertifikat, orgnummerBehandlingsansvarlig, orgnummerDatabehandler, orgnummerPostkasse, mottakerPersonnummer, mottakerPostkasse);   
-            }
-        }
-
         static void Main(string[] args)
         {
-           
-            
-            /*
+           /*
              * I dette eksemplet er det Posten som er den som produserer informasjon/brev/post som skal formidles (Behandlingsansvarlig),
              * Posten som er teknisk avsender, og det er Digipostkassen som skal motta meldingen. Derfor er alle organisasjonsnummer
              * identiske. 
              */
 
-            PostkasseSettings postkasseSettings = PostkasseSettings.GetPosten();
+            PostkasseInnstillinger postkasseInnstillinger = PostkasseInnstillinger.GetPosten();
 
             //Avsender
-            var behandlingsansvarlig = new Behandlingsansvarlig(new Organisasjonsnummer(postkasseSettings.OrgNummerBehandlingsansvarlig));
-            var tekniskAvsender = new Databehandler(postkasseSettings.OrgNummerDatabehandler, postkasseSettings.Avsendersertifikat);
+            var behandlingsansvarlig = new Behandlingsansvarlig(new Organisasjonsnummer(postkasseInnstillinger.OrgNummerBehandlingsansvarlig));
+            var tekniskAvsender = new Databehandler(postkasseInnstillinger.OrgNummerDatabehandler, postkasseInnstillinger.Avsendersertifikat);
 
             //Mottaker
-            var mottaker = new Mottaker(postkasseSettings.Personnummer, postkasseSettings.Postkasseadresse, postkasseSettings.Mottakersertifikat, postkasseSettings.OrgnummerPostkasse);
+            var mottaker = new Mottaker(postkasseInnstillinger.Personnummer, postkasseInnstillinger.Postkasseadresse, postkasseInnstillinger.Mottakersertifikat, postkasseInnstillinger.OrgnummerPostkasse);
 
             //Digital Post
             var digitalPost = new DigitalPost(mottaker, "Ikke-sensitiv tittel");
@@ -139,9 +46,7 @@ namespace SikkerDigitalPost.Testklient
 
             Transportkvittering transportkvittering = sikkerDigitalPostKlient.Send(forsendelse);
 
-
             var kjør = true;
-            int count = 0;
             while (kjør)
             {
                 //Hent kvittering
@@ -153,21 +58,16 @@ namespace SikkerDigitalPost.Testklient
                     kjør = false;
                    // throw new Exception("Denne meldingskøen er tom.");
                 }
-
-                count++;
-
-                //if (kvittering.GetType() == typeof(Feilmelding))
-                //{
-                //    throw new Exception("Du fikk en feiletkvittering, men det er ikke sikkert du genererte den nå nettopp.");
-                //}
+                
+                if (kvittering.GetType() == typeof(Feilmelding))
+                {
+                  // throw new Exception("Du fikk en feiletkvittering, men det er ikke sikkert du genererte den nå nettopp.");
+                }
                 
                 //Bekreft mottak av kvittering.
                 if(kjør)
                     sikkerDigitalPostKlient.Bekreft(kvittering);
             }
-
-            int i = 0;
-
         }
     }
 }
