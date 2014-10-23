6578c4c9ede6b89c44023e8edc928aba94fbdc8d
jgylta <jgylta@gmail.com>
2014-10-23 10:41:36 +0200

    Lagt til validering av referansers eksistens

--- SikkerDigitalPost.Klient/XmlValidering/Responsvalidator.cs
+++ SikkerDigitalPost.Klient/XmlValidering/Responsvalidator.cs
@@ -23,10 +23,15 @@ namespace SikkerDigitalPost.Klient.XmlValidering
             XmlNode responseRot = document.DocumentElement;
             var responseMgr = new XmlNamespaceManager(document.NameTable);
             responseMgr.AddNamespace("env", Navnerom.env);
+            responseMgr.AddNamespace("wsse", Navnerom.wsse);
             responseMgr.AddNamespace("ds", Navnerom.ds);
+            responseMgr.AddNamespace("eb", Navnerom.eb);
 
-            var signatureNode = (XmlElement)responseRot.SelectSingleNode("//ds:Signature", responseMgr);
+            var signatureNode = (XmlElement)responseRot.SelectSingleNode("/env:Envelope/env:Header/wsse:Security/ds:Signature", responseMgr);
             var signed = new SignedXmlWithAgnosticId(document);
+
+            ValiderInnhold(document, signatureNode, signed, responseMgr);
+
             signed.LoadXml(signatureNode);
             return signed.CheckSignature();
         }
@@ -54,6 +59,35 @@ namespace SikkerDigitalPost.Klient.XmlValidering
             var envelopeAsicDigest = envelopeRot.SelectSingleNode("//*[namespace-uri()='" + Navnerom.ds + "' and local-name()='Reference'][@URI = 'cid:" + guidHandler.DokumentpakkeId + "']", envelopeMgr).InnerText;
 
             return responseBodyDigest.Equals(envelopeBodyDigest) && responseAsicDigest.Equals(envelopeAsicDigest);
+        }
+
+        private void ValiderInnhold(XmlDocument response, XmlElement signature, SignedXmlWithAgnosticId signedXml, XmlNamespaceManager nsmgr)
+        {
+            string[] requiredSignatureElements = { "/env:Envelope/env:Header/wsse:Security/wsu:Timestamp", "/env:Envelope/env:Body", "/env:Envelope/env:Header/eb:Messaging" };
+
+            foreach (var elementXPath in requiredSignatureElements)
+            {
+                // Sørg for at svar inneholde påkrevede noder.
+                var nodes = response.SelectNodes(elementXPath, nsmgr);
+                if (nodes == null || nodes.Count == 0)
+                    throw new Exception(string.Format("Kan ikke finne påkrevet element '{0}' i svar fra meldingsformidler.", elementXPath));
+                if (nodes.Count > 1)
+                    throw new Exception(string.Format("Påkrevet element '{0}' kan kun forekomme én gang i svar fra meldingsformidler. Ble funnet {1} ganger.", elementXPath, nodes.Count));
+
+                // Sørg for at det finnes en refereanse til node i signatur element
+                var elementId = nodes[0].Attributes["Id"].Value;
+
+                var references = signature.SelectNodes(string.Format("./ds:SignedInfo/ds:Reference[URI='@{0}']", elementId), nsmgr);
+                if (references == null || references.Count == 0)
+                    throw new Exception(string.Format("Kan ikke finne påkrevet refereanse til element '{0}' i signatur fra meldingsformidler.", elementXPath));
+                if (references.Count > 1)
+                    throw new Exception(string.Format("Påkrevet refereanse til element '{0}' kan kun forekomme én gang i signatur fra meldingsformidler. Ble funnet {1} ganger.", elementXPath, references.Count));
+
+                // Sørg for at Id node matcher
+                var targetNode = signedXml.GetIdElement(response, elementId);
+                if (targetNode != nodes[0])
+                    throw new Exception(string.Format("Signaturreferansen med id '{0}' må refererer til node med sti '{1}'", elementId, elementXPath));
+            }
         }
     }
 }
