17dd7970741bd949c3e514166707798320477998
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2014-10-24 15:24:31 +0200

    Lagt til nuspec

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.6.*")]
-[assembly: AssemblyFileVersion("0.6.*")]
+[assembly: AssemblyVersion("0.9.*")]
+[assembly: AssemblyFileVersion("0.9.*")]
