7676bc6841249c079353e6bea5f60f2b489c2c60
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-02-12 11:59:03 +0100

    Satt versjon

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -14,5 +14,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.01.*")]
-[assembly: AssemblyFileVersion("1.01.*")]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.*")]
