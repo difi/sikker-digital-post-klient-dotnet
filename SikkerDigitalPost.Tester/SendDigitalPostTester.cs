591968f5c3660210c2705ad74aa9e95679a0a165
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2015-03-18 08:01:33 +0100

    Lagd en egen sertifikat-utility, dratt kode dit og ryddet i Integrasjonstester

--- SikkerDigitalPost.Tester/SendDigitalPostTester.cs
+++ SikkerDigitalPost.Tester/SendDigitalPostTester.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Management.Instrumentation;
@@ -7,6 +8,7 @@ using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Win32;
 using SikkerDigitalPost.Domene.Entiteter.Aktører;
+using SikkerDigitalPost.Domene.Entiteter.FysiskPost;
 using SikkerDigitalPost.Domene.Entiteter.Kvitteringer;
 using SikkerDigitalPost.Domene.Entiteter.Kvitteringer.Forretning;
 using SikkerDigitalPost.Domene.Entiteter.Kvitteringer.Transport;
@@ -22,6 +24,7 @@ namespace SikkerDigitalPost.Tester
     public class SendDigitalPostTester
     {
         private const string OrgnummerPosten = "984661185";
+        private const string MottakersertifikatThumbprint =  "B43CAAA0FBEE6C8DA85B47D1E5B7BCAB42AB9ADD";
 
         private TestContext testContextInstance;
         public TestContext TestContext
@@ -32,51 +35,25 @@ namespace SikkerDigitalPost.Tester
 
         [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @"|DataDirectory|\testdata\integrasjon\digitalpost.csv", "digitalpost#csv", DataAccessMethod.Sequential)]
         [TestMethod]
-        public void SendDigitalPost()
+        public void SendDigitalPostIntegrasjon()
         {
             var beskrivelse = "Ingen beskrivelse";
             try
             {
-                //Arrange
                 beskrivelse = GetColumnData("beskrivelse");
-                var personnummer = "04036125433"; //GetColumnData("personnummer");
+                Debug.WriteLine("Kjører test: " + beskrivelse);
+                var personnummer = "00000000000";
                 var postkasseadresse = GetColumnData("postkasseadresse");
-                var hoveddoktype = GetColumnData("hoveddokumenttype");
-                var hoveddokumentsti = GetFirstFile(GetColumnData("hoveddokument"));
-                var vedlegg1Type = GetColumnData("vedlegg1type");
-                var vedlegg1Sti = GetFirstFile(GetColumnData("vedlegg1"));
-                var vedlegg2Type = GetColumnData("vedlegg2type");
-                var vedlegg2Sti = GetFirstFile(GetColumnData("vedlegg2"));
+   
 
-                var mottaker = new DigitalPostMottaker(personnummer, postkasseadresse,
-                    MottakerSertifikat("B43CAAA0FBEE6C8DA85B47D1E5B7BCAB42AB9ADD"), OrgnummerPosten);
-                var postInfo = new DigitalPostInfo(mottaker, "Ikkesensitiv tittel fra Endetester", Sikkerhetsnivå.Nivå3,
-                    false);
+                var mottaker = new DigitalPostMottaker(personnummer, postkasseadresse, SertifikatUtility.MottakerSertifikat(MottakersertifikatThumbprint), OrgnummerPosten);
+                var postInfo = new DigitalPostInfo(mottaker, "Ikkesensitiv tittel fra Endetester", Sikkerhetsnivå.Nivå3, false);
                 var behandlingsansvarlig = new Behandlingsansvarlig(OrgnummerPosten);
                 behandlingsansvarlig.Avsenderidentifikator = "digipost";
-
-                var databehandler = new Databehandler(OrgnummerPosten,
-                    AvsenderSertifkat("8702F5E55217EC88CF2CCBADAC290BB4312594AC"));
-                var dokumentpakke = ByggDokumentpakke(hoveddokumentsti, hoveddoktype, vedlegg1Sti, vedlegg1Type,
-                    vedlegg2Sti, vedlegg2Type);
-
-                //Act
-                var sdpKlient = new SikkerDigitalPostKlient(databehandler, new Klientkonfigurasjon
-                {
-                    MeldingsformidlerUrl = new Uri("https://qaoffentlig.meldingsformidler.digipost.no/api/ebms")
-                });
-
-                var mpcId = "testerqueue-"+Guid.NewGuid();
-                var forsendelse = new Forsendelse(behandlingsansvarlig, postInfo, dokumentpakke, Prioritet.Prioritert,
-                    mpcId);
-                var transportkvittering = sdpKlient.Send(forsendelse);
-
-                //Assert
-                if (transportkvittering.GetType() == typeof (TransportFeiletKvittering))
-                {
-                    var feil = ((TransportFeiletKvittering) transportkvittering).Beskrivelse;
-                    Assert.Fail(feil);
-                }
+                var mpcId = "digitalpostintegrasjon-" + Guid.NewGuid();
+                
+                Forsendelse forsendelse;
+                var sdpKlient = ByggDokumentpakkeOgSend(behandlingsansvarlig, postInfo, mpcId, out forsendelse);
 
                 HentKvitteringOgBekreft(sdpKlient, beskrivelse, mpcId, forsendelse);
             }
@@ -87,6 +64,75 @@ namespace SikkerDigitalPost.Tester
 
         }
 
+        private SikkerDigitalPostKlient ByggDokumentpakkeOgSend(Behandlingsansvarlig behandlingsansvarlig,
+            PostInfo postInfo, string mpcId, out Forsendelse forsendelse)
+        {
+            var hoveddoktype = GetColumnData("hoveddokumenttype");
+            var hoveddokumentsti = GetFirstFile(GetColumnData("hoveddokument"));
+            var vedlegg1Type = GetColumnData("vedlegg1type");
+            var vedlegg1Sti = GetFirstFile(GetColumnData("vedlegg1"));
+            var vedlegg2Type = GetColumnData("vedlegg2type");
+            var vedlegg2Sti = GetFirstFile(GetColumnData("vedlegg2"));
+
+            var databehandler = new Databehandler(OrgnummerPosten,
+                SertifikatUtility.AvsenderSertifkat("8702F5E55217EC88CF2CCBADAC290BB4312594AC"));
+            var dokumentpakke = ByggDokumentpakke(hoveddokumentsti, hoveddoktype, vedlegg1Sti, vedlegg1Type, vedlegg2Sti,
+                vedlegg2Type);
+
+            var sdpKlient = new SikkerDigitalPostKlient(databehandler, new Klientkonfigurasjon
+            {
+                MeldingsformidlerUrl = new Uri("https://qaoffentlig.meldingsformidler.digipost.no/api/ebms")
+            });
+
+            forsendelse = new Forsendelse(behandlingsansvarlig, postInfo, dokumentpakke, Prioritet.Prioritert, mpcId);
+            var transportkvittering = sdpKlient.Send(forsendelse);
+
+            if (transportkvittering.GetType() == typeof (TransportFeiletKvittering))
+            {
+                var feil = ((TransportFeiletKvittering) transportkvittering).Beskrivelse;
+                Assert.Fail(feil);
+            }
+            return sdpKlient;
+        }
+        
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @"|DataDirectory|\testdata\integrasjon\fysiskpost.csv", "fysiskpost#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void SendFysiskPostIntegrasjon()
+        {
+            var beskrivelse = "Ingen beskrivelse";
+            try
+            {
+
+                beskrivelse = GetColumnData("beskrivelse");
+                Debug.WriteLine("Kjører test: " + beskrivelse);
+                var mottakernavn = "Ano Nym";
+                var mottakerAdresse = new NorskAdresse("1337", "Sandvika") { Adresselinje1 = "Peppern Gror 32" };
+                var returAdresse = new NorskAdresse("4242", "Returveien") { Adresselinje1 = "Veien til Nav 43" };
+
+                var posttype = (Posttype)Enum.Parse(typeof(Posttype), GetColumnData("Posttype"), ignoreCase: true);
+                var utskriftsfarge = (Utskriftsfarge)Enum.Parse(typeof(Utskriftsfarge), GetColumnData("utskriftsfarge"), ignoreCase: true);
+                var posthåndtering = (Posthåndtering)Enum.Parse(typeof(Posthåndtering), GetColumnData("posthandtering"), ignoreCase: true);
+
+                var mottaker = new FysiskPostMottaker(mottakernavn, mottakerAdresse, SertifikatUtility.MottakerSertifikat(MottakersertifikatThumbprint), OrgnummerPosten);
+                var returmottaker = new FysiskPostMottaker("Returkongen", returAdresse);
+                var postinfo = new FysiskPostInfo(mottaker, posttype, utskriftsfarge, posthåndtering, returmottaker);
+
+                var behandlingsansvarlig = new Behandlingsansvarlig(OrgnummerPosten);
+                behandlingsansvarlig.Avsenderidentifikator = "digipost";
+                var mpcId = "fysiskpostintegrasjon-" + Guid.NewGuid();
+
+                Forsendelse forsendelse;
+                var sdpKlient = ByggDokumentpakkeOgSend(behandlingsansvarlig, postinfo, mpcId, out forsendelse);
+
+                HentKvitteringOgBekreft(sdpKlient, beskrivelse, mpcId, forsendelse);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Feilet for '{0}', feilmelding: {1}, {2}", beskrivelse, e.Message, e.StackTrace);
+            }
+
+        }
+        
         private static void HentKvitteringOgBekreft(SikkerDigitalPostKlient sdpKlient, string beskrivelse, string mpcId,
             Forsendelse forsendelse)
         {
@@ -94,7 +140,7 @@ namespace SikkerDigitalPost.Tester
 
             while (hentKvitteringPåNytt)
             {
-                Thread.Sleep(500);
+                Thread.Sleep(1000);
                 var kvitteringsforespørsel = new Kvitteringsforespørsel(Prioritet.Prioritert, mpcId);
                 var kvittering = sdpKlient.HentKvittering(kvitteringsforespørsel);
 
@@ -112,7 +158,7 @@ namespace SikkerDigitalPost.Tester
 
                 if (kvittering is Feilmelding)
                 {
-                    var feilmelding = (Feilmelding) kvittering;
+                    var feilmelding = (Feilmelding)kvittering;
                     konversasjonsId = feilmelding.KonversasjonsId;
                     Assert.Fail(String.Format("Test '{0}' feilet. Feilmelding fra Meldingsformidler: {1}", beskrivelse,
                         feilmelding.Detaljer));
@@ -120,8 +166,14 @@ namespace SikkerDigitalPost.Tester
 
                 if (kvittering is Leveringskvittering)
                 {
-                    var leveringskvittering = (Leveringskvittering) kvittering;
+                    var leveringskvittering = (Leveringskvittering)kvittering;
                     konversasjonsId = leveringskvittering.KonversasjonsId;
+                }
+
+                if (kvittering is Mottakskvittering)
+                {
+                    var mottakskvittering = (Mottakskvittering)kvittering;
+                    konversasjonsId = mottakskvittering.KonversasjonsId;
                 }
 
                 if (konversasjonsId.ToString() != forsendelse.KonversasjonsId.ToString())
@@ -132,7 +184,7 @@ namespace SikkerDigitalPost.Tester
                             "eller har noe skjedd galt med hvilken kø du henter fra?", beskrivelse));
                 }
 
-                sdpKlient.Bekreft((Forretningskvittering) kvittering);
+                sdpKlient.Bekreft((Forretningskvittering)kvittering);
             }
         }
 
@@ -145,12 +197,12 @@ namespace SikkerDigitalPost.Tester
                 throw new FileNotFoundException(String.Format("Kunne ikke finne filen med ressurssti {0}, har du lagt den i 'testdata' og valgt BuildAction = 'Embedded Resource'? " +
                                                               "Husk at stiseparator SKAL være punktum. Dette er ikke en feil.", path));
             }
-            
+
             return elements.ElementAt(0);
         }
 
         private static Dokumentpakke ByggDokumentpakke(string hoveddokumentsti, string hoveddoktype, string vedlegg1Sti,
-            string vedlegg1Type, string vedlegg2Sti,string vedlegg2Type)
+            string vedlegg1Type, string vedlegg2Sti, string vedlegg2Type)
         {
             var hoveddokumentBytes = ResourceUtility.ReadAllBytes(false, hoveddokumentsti);
             var hoveddokument = new Dokument(DateTime.Now.ToString("G") + " - Hoveddokument", hoveddokumentBytes,
@@ -163,8 +215,7 @@ namespace SikkerDigitalPost.Tester
             return dokumentpakke;
         }
 
-        private static void LeggVedleggTilDokumentpakke(Dokumentpakke dokumentpakke, string tittel, string sti, string innholdstype, string språkkode,
-            string filnavn)
+        private static void LeggVedleggTilDokumentpakke(Dokumentpakke dokumentpakke, string tittel, string sti, string innholdstype, string språkkode, string filnavn)
         {
             if (String.IsNullOrEmpty(sti) || String.IsNullOrEmpty(innholdstype))
                 return;
@@ -178,42 +229,5 @@ namespace SikkerDigitalPost.Tester
         {
             return TestContext.DataRow[column].ToString();
         }
-
-        private static X509Certificate2 AvsenderSertifkat(string hash)
-        {
-            X509Store storeMy = new X509Store(StoreName.My, StoreLocation.CurrentUser);
-            X509Certificate2 tekniskAvsenderSertifikat;
-            try
-            {
-                storeMy.Open(OpenFlags.ReadOnly);
-                tekniskAvsenderSertifikat = storeMy.Certificates.Find(
-                    X509FindType.FindByThumbprint, hash, true)[0];
-            }
-            catch (Exception e)
-            {
-                throw new InstanceNotFoundException("Kunne ikke finne avsendersertifikat for testing. Har du lagt det til slik guiden tilsier? (https://github.com/difi/sikker-digital-post-net-klient#legg-inn-avsendersertifikat-i-certificate-store) ", e);
-            }
-            storeMy.Close();
-            return tekniskAvsenderSertifikat;
-        }
-
-        private static X509Certificate2 MottakerSertifikat(string hash)
-        {
-            var storeTrusted = new X509Store(StoreName.TrustedPeople, StoreLocation.CurrentUser);
-            X509Certificate2 mottakerSertifikat;
-            try
-            {
-                storeTrusted.Open(OpenFlags.ReadOnly);
-                mottakerSertifikat =
-                    storeTrusted.Certificates.Find(X509FindType.FindByThumbprint, hash, true)[0];
-            }
-            catch (Exception e)
-            {
-                throw new InstanceNotFoundException("Kunne ikke finne mottakersertifikat for testing. Har du lagt det til slik guiden tilsier? (https://github.com/difi/sikker-digital-post-net-klient#legg-inn-mottakersertifikat-i-certificate-store) ", e);
-            }
-            storeTrusted.Close();
-            return mottakerSertifikat;
-        }
-
     }
 }
