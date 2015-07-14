c10e701fc61e8e1869d6bd761cbe4a2deaeebc17
Kristian Enge <kristian.ienge@gmail.com>
2015-07-14 10:08:59 +0200

    FysiskPostInfoTest

---
+++ Difi.SikkerDigitalPost.Klient.Tester/enhetstester/FysiskPostInfoTest.cs
@@ -0,0 +1,24 @@
+﻿using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.FysiskPost;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Post;
+using Difi.SikkerDigitalPost.Klient.Domene.Enums;
+using Difi.SikkerDigitalPost.Klient.Tester.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Difi.SikkerDigitalPost.Klient.Tester.enhetstester
+{
+    [TestClass]
+    public class FysiskPostInfoTest
+    {
+        [TestMethod]
+        public void FysiskPostMottaker_skalKunnebliBruktSomMottakerOgRetur()
+        {
+           var fysiskPostInfo =
+             new FysiskPostInfo(DomeneUtility.GetFysiskPostMottaker(), Posttype.A, Utskriftsfarge.Farge,
+                Posthåndtering.DirekteRetur, DomeneUtility.GetFysiskPostMottaker());
+            
+            Assert.IsInstanceOfType(fysiskPostInfo.Mottaker,typeof( FysiskPostMottaker));
+            Assert.IsInstanceOfType(fysiskPostInfo.ReturMottaker, typeof(FysiskPostMottaker));
+        }
+
+    }
+}
