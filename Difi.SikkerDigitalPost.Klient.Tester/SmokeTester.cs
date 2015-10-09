8aa498fba30de6580470f5a61e6a68eab172e84e
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2015-10-09 12:50:22 +0200

    Fjerner c#6-formatering av string

--- Difi.SikkerDigitalPost.Klient.Tester/SmokeTester.cs
+++ Difi.SikkerDigitalPost.Klient.Tester/SmokeTester.cs
@@ -115,8 +115,9 @@ namespace Difi.SikkerDigitalPost.Klient.Tester
                 if (konversasjonsId != forsendelse.KonversasjonsId)
                 {
                     throw new FieldAccessException(
-                        $"Fikk ikke til å hente kvittering for test '{testBeskrivelse}' -- det ble hentet feil kvittering eller ingen kvittering. Var du for rask å hente, " +
-                        "eller har noe skjedd galt med hvilken kø du henter fra?");
+                        string.Format(
+                            "Fikk ikke til å hente kvittering for test '{0}' -- det ble hentet feil kvittering eller ingen kvittering. Var du for rask å hente, " +
+                            "eller har noe skjedd galt med hvilken kø du henter fra?", testBeskrivelse));
                 }
             }
             return kvittering;
