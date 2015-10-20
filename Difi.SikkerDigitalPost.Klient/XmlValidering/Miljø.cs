fa433a1b9e0baab4ab3c15bc45193b5e556efd54
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2015-10-20 09:42:19 +0200

    Refaktorerer ResponsvalidatorTester

--- Difi.SikkerDigitalPost.Klient/XmlValidering/Miljø.cs
+++ Difi.SikkerDigitalPost.Klient/XmlValidering/Miljø.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Security.Cryptography.X509Certificates;
-using System.Security.Policy;
 using Difi.SikkerDigitalPost.Klient.Utilities;
 
 namespace Difi.SikkerDigitalPost.Klient.XmlValidering
