b17e43ebc84b4dd696d12439fc8478142a9a23d6
Kristian Enge <kristian.ienge@gmail.com>
2015-11-09 17:04:16 +0100

    levering- og åpningskvittering

---
+++ Difi.SikkerDigitalPost.Klient.Tester/Entiteter/Kvitteringer/Forretning/LeveringskvitteringTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Kvitteringer.Forretning;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Kvitteringer.Forretning.Tests
+{
+    [TestClass()]
+    public class LeveringskvitteringTests
+    {
+        [TestClass]
+        public class KonstruktørMethod : LeveringskvitteringTests
+        {
+            [TestMethod]
+            public void EnkelKonstruktør()
+            {
+            }
+        }
+    }
+}
