0ff220aef25733470fe6aca1f26564d065ae97a0
Kristian Enge <kristian.ienge@gmail.com>
2015-11-10 10:56:15 +0100

    mottakskvittering og returpostkvittering

---
+++ Difi.SikkerDigitalPost.Klient.Tester/Entiteter/Kvitteringer/Forretning/ReturpostkvitteringTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Kvitteringer.Forretning.Tests
+{
+    [TestClass()]
+    public class ReturpostkvitteringTests
+    {
+        [TestClass]
+        public class KonstruktørMethod : ReturpostkvitteringTests
+        {
+            [TestMethod]
+            public void EnkelKonstruktør()
+            {
+                //TODO: MAKE UNIT TEST
+                Assert.IsTrue(1==2);
+            }
+        }
+    }
+}
