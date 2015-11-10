418ea15ab1b71333bc7e6e6dcf1c28778b4d14f5
Kristian Enge <kristian.ienge@gmail.com>
2015-11-10 12:50:56 +0100

    La til tomkøkvittering istedet for å svare med null. (breaking change)

---
+++ Difi.SikkerDigitalPost.Klient.Domene/Entiteter/Kvitteringer/TomKøKvittering.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Kvitteringer.Forretning;
+
+namespace Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Kvitteringer
+{
+    public class TomKøKvittering : Kvittering
+    {
+        internal TomKøKvittering(XmlDocument xmlDocument, XmlNamespaceManager namespaceManager)
+            : base(xmlDocument, namespaceManager)
+        {
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} med meldingsId {1}: \nTidspunkt: {2}. \nRefererer til melding med id: {4}",
+                GetType().Name, MeldingsId, Tidspunkt, ReferanseTilMeldingId);
+        }
+    }
+}
