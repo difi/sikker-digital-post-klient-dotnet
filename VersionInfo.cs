6733aeed3597496ed62a77c45e1a5b10d896277d
Kristian Enge <kristian.ienge@gmail.com>
2015-11-23 16:59:28 +0100

    setter versjon til 1.4

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -13,5 +13,5 @@
  */
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.3.*")]
-[assembly: AssemblyFileVersion("1.3.*")]
+[assembly: AssemblyVersion("1.4.*")]
+[assembly: AssemblyFileVersion("1.4.*")]
