51fd79131c69de18024e79b524da5e7653be19ea
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-01-14 08:43:27 +0100

    Simplifies ToStringWithUtcOffset
    Uses .ToString with formatting "O" as this is ISO8601 with UTC offset.

--- Difi.SikkerDigitalPost.Klient/Extensions/DateTimeExtensions.cs
+++ Difi.SikkerDigitalPost.Klient/Extensions/DateTimeExtensions.cs
@@ -6,18 +6,7 @@ namespace Difi.SikkerDigitalPost.Klient.Extensions
     {
         public static string ToStringWithUtcOffset(this DateTime dateTime)
         {
-            const string fullFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff";
-
-            string date = dateTime.ToString(fullFormat);
-
-            TimeZoneInfo timeZone = TimeZoneInfo.Local;
-            TimeSpan offset = timeZone.GetUtcOffset(dateTime);
-
-            var format = @"hh':'mm";
-            var fortegn = offset.CompareTo(TimeSpan.Zero) >= 0 ? "'+'" : "'-'";
-            format = String.Format("{0}{1}", fortegn, format);
-            
-            return dateTime.ToString(String.Format("{0}{1}", date, offset.ToString(format)));
+            return dateTime.ToString("O");
         }
 
     }
