1f1e4c990b31f9f2dc0d28b7b5749ebaefb06156
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-01-29 12:16:44 +0100

    Legger til tom klasse for KvitteringParser og -Tester
    Klargjøring til tester.

---
+++ Difi.SikkerDigitalPost.Klient.Tester/KvitteringParserTester.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Difi.SikkerDigitalPost.Klient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difi.SikkerDigitalPost.Klient.Tests
+{
+    [TestClass()]
+    public class KvitteringParserTester
+    {
+        [TestClass]
+        public class TilKvitteringMethod : KvitteringParserTester
+        {
+            [TestMethod]
+            public void ParserVarslingFeiletKvittering()
+            {
+                //Arrange
+                
+                //Act
+
+                //Assert
+            }
+
+            [TestMethod]
+            public void ParserLeveringskvittering()
+            {
+                //Arrange
+                
+
+                //Act
+
+                //Assert
+            }
+
+            [TestMethod]
+            public void ParserMottakskvittering()
+            {
+                //Arrange
+                
+
+                //Act
+
+                //Assert
+            }
+
+            [TestMethod]
+            public void ParserReturpostkvittering()
+            {
+                //Arrange
+                
+
+                //Act
+
+                //Assert
+            }
+        }
+       
+    }
+}
---
+++ Difi.SikkerDigitalPost.Klient/KvitteringParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Kvitteringer.Forretning;
+
+namespace Difi.SikkerDigitalPost.Klient
+{
+    public class KvitteringParser
+    {
+        public static VarslingFeiletKvittering TilVarslingFeiletKvittering(XmlDocument varslingFeiletXmlDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Leveringskvittering TilLeveringskvittering(XmlDocument leveringskvitteringXmlDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Mottakskvittering TilMottakskvittering(XmlDocument mottakskvitteringXmlDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Returpostkvittering TilReturpostkvittering(XmlDocument returpostkvitteringXmlDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Åpningskvittering TilÅpningskvittering(XmlDocument åpningskvitteringXmlDocument)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
