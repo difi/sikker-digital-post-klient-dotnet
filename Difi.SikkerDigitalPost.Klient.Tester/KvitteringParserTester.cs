dbd6dad0b57f2f9f78e3b77215d33e3baef26b96
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-01-29 12:50:03 +0100

    Legger til lasting av xmlfiler i tester

--- Difi.SikkerDigitalPost.Klient.Tester/KvitteringParserTester.cs
+++ Difi.SikkerDigitalPost.Klient.Tester/KvitteringParserTester.cs
@@ -5,30 +5,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using ApiClientShared;
+using Difi.Felles.Utility.Utilities;
 
 namespace Difi.SikkerDigitalPost.Klient.Tests
 {
     [TestClass()]
     public class KvitteringParserTester
     {
+        ResourceUtility ResourceUtility = new ResourceUtility("Difi.SikkerDigitalPost.Klient.Tester.Skjema.Eksempler.Kvitteringer");
+
         [TestClass]
         public class TilKvitteringMethod : KvitteringParserTester
         {
             [TestMethod]
-            public void ParserVarslingFeiletKvittering()
-            {
-                //Arrange
-                
-                //Act
-
-                //Assert
-            }
-
-            [TestMethod]
             public void ParserLeveringskvittering()
             {
                 //Arrange
-                
+                var varslingFeiletKvittering = TilXmlDokument("Leveringskvittering.xml");
 
                 //Act
 
@@ -39,7 +34,8 @@ namespace Difi.SikkerDigitalPost.Klient.Tests
             public void ParserMottakskvittering()
             {
                 //Arrange
-                
+                var varslingFeiletKvittering = TilXmlDokument("Mottakskvittering.xml");
+
 
                 //Act
 
@@ -50,13 +46,41 @@ namespace Difi.SikkerDigitalPost.Klient.Tests
             public void ParserReturpostkvittering()
             {
                 //Arrange
-                
+                var varslingFeiletKvittering = TilXmlDokument("Returpostkvittering.xml");
 
                 //Act
 
                 //Assert
             }
+
+            [TestMethod]
+            public void ParserVarslingFeiletKvittering()
+            {
+                //Arrange
+                var varslingFeiletKvittering = TilXmlDokument("VarslingFeiletKvittering.xml");
+
+                //Act
+
+                //Assert
+            }
+
+            private XmlDocument TilXmlDokument(string kvittering)
+            {
+                return XmlUtility.TilXmlDokument(Encoding.UTF8.GetString(ResourceUtility.ReadAllBytes(true, kvittering)));
+            }
+
+            [TestMethod]
+            public void ParserÅpningskvittering()
+            {
+                //Arrange
+                //XmlUtility.TilXmlDokument(ResourceUtility.ReadAllBytes(true, k));
+
+                //Act
+
+                //Assert
+            }
+
         }
-       
+
     }
 }
