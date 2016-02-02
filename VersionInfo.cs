4c5c5ce75c38062ab817af02dfec5cb447d5fac3
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-02-02 17:27:19 +0100

    Oppgraderer versjon til 1.5 for å indikere ny funskjonalitet og en del bugfikser

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.4.*")]
-[assembly: AssemblyFileVersion("1.4.*")]
+[assembly: AssemblyVersion("1.5.*")]
+#pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
+[assembly: AssemblyFileVersion("1.5.*")]
+#pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
