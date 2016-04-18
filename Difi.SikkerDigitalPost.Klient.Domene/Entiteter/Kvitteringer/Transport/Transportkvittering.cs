da97bf39033ed6c5378f8a661dcd945749d421fd
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-04-18 09:37:49 +0200

    Legger til beskrivelse av bytes

--- Difi.SikkerDigitalPost.Klient.Domene/Entiteter/Kvitteringer/Transport/Transportkvittering.cs
+++ Difi.SikkerDigitalPost.Klient.Domene/Entiteter/Kvitteringer/Transport/Transportkvittering.cs
@@ -7,6 +7,11 @@
         {
         }
 
+        /// <summary>
+        /// Angir hvor mange bytes som er i dokumentpakken som ble sendt i henhold til Difis spesifikasjon: 
+        /// http://begrep.difi.no/SikkerDigitalPost/1.2.1/forretningslag/meldingsstorrelse og 
+        /// http://begrep.difi.no/SikkerDigitalPost/1.2.1/forretningslag/Dokumentpakke/langtidslagring 
+        /// </summary>
         public long AntallBytesDokumentpakke { get; set; }
     }
 }
