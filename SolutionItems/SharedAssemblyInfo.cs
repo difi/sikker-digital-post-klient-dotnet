f4b5dc638b2e04ab4cf518a29a15064371c5b6ac
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-04-26 14:12:40 +0200

    Oppdaterer versjon til å være 1.6. Det nye loggregimet er breaking change

--- SolutionItems/SharedAssemblyInfo.cs
+++ SolutionItems/SharedAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.*")]
+[assembly: AssemblyVersion("1.6.*")]
 #pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
 
-[assembly: AssemblyFileVersion("1.5.*")]
+[assembly: AssemblyFileVersion("1.6.*")]
 #pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
