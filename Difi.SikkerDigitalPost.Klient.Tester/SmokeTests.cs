e554666a8b1c549ad42322137b552904fc08d602
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-09-23 09:15:12 +0200

    Gjør røyktestene flytende
    Smoketestene har vært gode å ha, men har vært svært uryddige. Har nå gjort de om til å være `fluent`. Det leser bedre og gir mindre duplisering. Win-win!

---
+++ Difi.SikkerDigitalPost.Klient.Tester/SmokeTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Difi.SikkerDigitalPost.Klient.Api;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Aktører;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Kvitteringer;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Kvitteringer.Forretning;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Kvitteringer.Transport;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter.Post;
+using Difi.SikkerDigitalPost.Klient.Domene.Enums;
+using Difi.SikkerDigitalPost.Klient.Tester.Utilities;
+using Difi.SikkerDigitalPost.Klient.XmlValidering;
+using Xunit;
+
+namespace Difi.SikkerDigitalPost.Klient.Tester
+{
+    public class SmokeTests
+    {
+        private SmokeTestsHelper _t;
+
+        public SmokeTests()
+        {
+            var miljø = Miljø.FunksjoneltTestmiljø;
+
+            _t = new SmokeTestsHelper(miljø);
+        }
+
+        [Fact]
+        public void Send_digital_with_multiple_documents()
+        {
+            _t
+                .Create_Digital_Forsendelse_with_multiple_documents()
+                .Send()
+                .Expect_Message_Response_To_Be_TransportOkKvittering()
+                .Fetch_Receipt()
+                .ConfirmReceipt();
+        }
+
+        [Fact]
+        public void Send_physical()
+        {
+            _t
+                .Create_Physical_Forsendelse()
+                .Send()
+                .Expect_Message_Response_To_Be_TransportOkKvittering()
+                .Fetch_Receipt()
+                .ConfirmReceipt();
+        }
+
+        [Fact]
+        public void Send_on_behalf_of()
+        {
+            _t
+                .Create_digital_forsendelse_with_different_sender()
+                .Send()
+                .Expect_Message_Response_To_Be_TransportOkKvittering()
+                .Fetch_Receipt()
+                .ConfirmReceipt();
+        }
+    }
+}
