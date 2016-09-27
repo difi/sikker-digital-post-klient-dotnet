57fbeb604ac1a0ab22268a6df98b77ccc99bcc6a
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-09-27 17:10:20 +0200

    Oppdaterer .nuspec til å reflektere ny versjon av difi-felles
    - Og oversetter noen metoder i SmokeTester

--- Difi.SikkerDigitalPost.Klient.Domene/Extensions/EnumExtensions.cs
+++ Difi.SikkerDigitalPost.Klient.Domene/Extensions/EnumExtensions.cs
@@ -3,23 +3,22 @@ using Difi.Felles.Utility;
 
 namespace Difi.SikkerDigitalPost.Klient.Domene.Extensions
 {
-    public static class EnumExtensions
+    internal static class EnumExtensions
     {
-
-    public static string ToNorwegianString(this CertificateValidationType certificateValidationType)
-    {
-        switch (certificateValidationType)
+        public static string ToNorwegianString(this CertificateValidationType certificateValidationType)
         {
-            case CertificateValidationType.Valid:
-                return "Gyldig";
-            case CertificateValidationType.InvalidCertificate:
-                return "UgyldigSertifikat";
-            case CertificateValidationType.InvalidChain:
-                return "UgyldigKjede";
-            default:
-                throw new ArgumentOutOfRangeException();
+            switch (certificateValidationType)
+            {
+                case CertificateValidationType.Valid:
+                    return "Gyldig";
+                case CertificateValidationType.InvalidCertificate:
+                    return "UgyldigSertifikat";
+                case CertificateValidationType.InvalidChain:
+                    return "UgyldigKjede";
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
-    }
 
     }
 }
