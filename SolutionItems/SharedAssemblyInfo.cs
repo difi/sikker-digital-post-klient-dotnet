df7b1bfd7afed0eb9160dcb6f6b546ffb0053f43
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-11-03 19:52:42 +0100

    # This is a combination of 13 commits. # The first commit's message is: Legger til initiell appveyor
    # This is the 2nd commit message:
    
    Fikser https url for appveyor submodul
    
    # This is the 3rd commit message:
    
    Fikser rett url for https appveyor
    
    # This is the 4th commit message:
    
    Rydder opp i AssemblyInfo
    
    Flytter fellesvariabler til SharedAssemblyInfo
    
    # This is the 5th commit message:
    
    Fikser .nuspec
    
    # This is the 6th commit message:
    
    Setter ny signingkey og fjerner x509tester
    
    # This is the 7th commit message:
    
    Oppdaterer og legger til manglende Http.Formatting.Extension
    
    # This is the 8th commit message:
    
    Endrer thumbprinttester til å hente fra LocalMachine
    
    Det er bare der vi har et sertifikat
    
    # This is the 9th commit message:
    
    Pusher opp manglende fil
    
    # This is the 10th commit message:
    
    Legger til manglende sluttklamme i nuspec
    
    # This is the 11th commit message:
    
    Legger til ny kryptert api-key
    
    # This is the 12th commit message:
    
    Bumper versjon til 1.21 for å se om det er rett nr vi leser av
    
    # This is the 13th commit message:
    
    Oppdaterer submodul for debugging av patching

--- SolutionItems/SharedAssemblyInfo.cs
+++ SolutionItems/SharedAssemblyInfo.cs
@@ -1,7 +1,11 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.10.2.*")]
-
-#pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
-[assembly: AssemblyFileVersion("1.10.2.*")]
-#pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
+[assembly: AssemblyCompany("Direktoratet for forvaltning og IKT (Difi)")]
+[assembly: AssemblyTrademark("Direktoratet for forvaltning og IKT (Difi)")]
+[assembly: AssemblyProduct("Difi Opppslagstjeneste Klient")]
+[assembly: AssemblyDescription("Klientbibliotek for integrasjon mot Oppslagstjenesten for kontakt og reservasjonregisteret")]
+[assembly: AssemblyVersion("1.21.0")]
+[assembly: AssemblyFileVersion("1.21.0")]
+[assembly: AssemblyInformationalVersion("1.11.0")]
+[assembly: AssemblyCopyright("© 2015-2016 Direktoratet for forvaltning og IKT (Difi)")]
+[assembly: AssemblyCulture("")]
