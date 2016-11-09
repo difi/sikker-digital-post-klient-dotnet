3d0e6f7f537a72b148cc5e2f95c83c45df8f26ad
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-11-09 10:10:38 +0100

    Rydder opp i AssemblyInfo
    Flytter fellesvariabler til SharedAssemblyInfo

--- Difi.SikkerDigitalPost.Klient.Resources/Properties/AssemblyInfo.cs
+++ Difi.SikkerDigitalPost.Klient.Resources/Properties/AssemblyInfo.cs
@@ -2,26 +2,8 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
 [assembly: AssemblyTitle("Difi.SikkerDigitalPost.Klient.Resources")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Difi.SikkerDigitalPost.Klient.Resources")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("dc581ec3-ca16-473b-bdd5-07b30a8b4f05")]
 [assembly: InternalsVisibleTo("Difi.SikkerDigitalPost.Klient.Tester,PublicKey=00240000048000009400000006020000002400005253413100040000010001008b3388f9c416425f0145bbcf26e66b9a87c4e08b4cd41563e4bc8846df38ba4d997c5408cc77da26d79b03c39874a6af9df0aff3e7bdb3c0e53a91f6d19c50e160f5bf67986a04f0f985eca0252f557ed9ae520dd51e3107d6168d073d4ec5ada28d34e492ad9fb7af29c82309c5c0124211e679caea38d5463d2af9042dafda")]
--- Difi.SikkerDigitalPost.Klient.Testklient/Properties/AssemblyInfo.cs
+++ Difi.SikkerDigitalPost.Klient.Testklient/Properties/AssemblyInfo.cs
@@ -1,25 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
 [assembly: AssemblyTitle("SikkerDigitalPost.Net.KlientDemo")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SikkerDigitalPost.Net.KlientDemo")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("1b4bdafb-d221-47d7-98ad-be0b62ee64eb")]
