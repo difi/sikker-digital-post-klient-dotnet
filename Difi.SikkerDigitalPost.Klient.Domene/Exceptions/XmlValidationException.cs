748eb9b96b99956db39705d74eab2102e703e1ab
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-11-23 08:24:13 +0100

    SdpSecurityException -> SecurityException

--- Difi.SikkerDigitalPost.Klient.Domene/Exceptions/XmlValidationException.cs
+++ Difi.SikkerDigitalPost.Klient.Domene/Exceptions/XmlValidationException.cs
@@ -4,13 +4,12 @@ using System.Collections.Generic;
 namespace Difi.SikkerDigitalPost.Klient.Domene.Exceptions
 {
     [Serializable]
-    public class XmlValidationException : KonfigurasjonsException
+    public class XmlValidationException : SikkerDigitalPostException
     {
         public XmlValidationException(string message, List<string> validationMessages)
             : base(message)
         {
             ValidationMessages = validationMessages;
-
         }
 
         public XmlValidationException(string message, Exception inner)
