00e36df5c448d7960e0a1ea490da6af799f3fb51
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-11-23 15:26:46 +0100

    Flytter CertificateValidationProperties til egen fil

---
+++ Difi.SikkerDigitalPost.Klient/XmlValidering/CertificateValidationProperties.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Difi.SikkerDigitalPost.Klient.Domene.Entiteter;
+
+namespace Difi.SikkerDigitalPost.Klient.XmlValidering
+{
+    internal class CertificateValidationProperties
+    {
+        public CertificateValidationProperties(X509Certificate2Collection allowedChainCertificates, Organisasjonsnummer organisasjonsnummerMeldingsformidler)
+        {
+            AllowedChainCertificates = allowedChainCertificates;
+            OrganisasjonsnummerMeldingsformidler = organisasjonsnummerMeldingsformidler;
+        }
+
+        public X509Certificate2Collection AllowedChainCertificates { get; }
+
+        public Organisasjonsnummer OrganisasjonsnummerMeldingsformidler { get; }
+    }
+}
