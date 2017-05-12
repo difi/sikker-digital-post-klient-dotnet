2be5a4329a16411d5cfe5b592a246906c705585e
Steinar Henriksen <steinar@rocketfarm.no>
2017-05-12 00:46:39 -0700

    Updated messages used for unit tests that were signed with now expired certificates and therefore causing tests to fail

--- Difi.SikkerDigitalPost.Klient.Tester/XmlValidering/ResponseValidatorTests.cs
+++ Difi.SikkerDigitalPost.Klient.Tester/XmlValidering/ResponseValidatorTests.cs
@@ -55,9 +55,9 @@ namespace Difi.SikkerDigitalPost.Klient.Tester.XmlValidering
                 var responseValidator = new ResponseValidator(sentMessage, XmlUtility.TilXmlDokument(TransportKvittering.TransportOkKvittertingFunksjoneltTestmiljø), certificateValidationProperties);
                 var guidUtility = new GuidUtility
                 {
-                    BinarySecurityTokenId = "X509-513ffecb-cd7e-4bb3-a4c5-47eff314683f",
+                    BinarySecurityTokenId = "X509-368aad63-6d28-44e1-864a-a7b31908c67a",
                     BodyId = "soapBody",
-                    DokumentpakkeId = "4fa27c07-8a0f-45a9-954e-c658f6c480af@meldingsformidler.sdp.difi.no",
+                    DokumentpakkeId = "74b77e2b-f409-4954-a2df-fdfa462d2339@meldingsformidler.sdp.difi.no",
                     EbMessagingId = "id-68ae7123-bf5c-4d15-835c-4a6b91106e77",
                     MessageId = "388214db-29cc-43c7-9543-877e017e5bb4",
                     TimestampId = "TS-76740c34-88d2-4bb6-82d2-9e9f0e474087"
