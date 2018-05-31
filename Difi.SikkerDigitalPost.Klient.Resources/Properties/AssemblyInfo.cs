7976b63c68d31dc568375dfabf188f8a03c6f719
Sindre Nordbø <sindre.nordbo@bekk.no>
2018-05-31 09:03:12 +0200

    Fjerner strong name signing
    Fixes #205

--- Difi.SikkerDigitalPost.Klient.Resources/Properties/AssemblyInfo.cs
+++ Difi.SikkerDigitalPost.Klient.Resources/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 [assembly: Guid("dc581ec3-ca16-473b-bdd5-07b30a8b4f05")]
-[assembly: InternalsVisibleTo("Difi.SikkerDigitalPost.Klient.Tester,PublicKey=0024000004800000940000000602000000240000525341310004000001000100ff5715ef9a7b09e7e5a9927f67ac8abbe78bda137f8ae6737bab4cf1c3e91d10f9e964ca7ceb3ae4b654f26b7a8f7f5dda0d5f12d5cab26663cabcf987462d8b9825b66d9118503c4871f799249e256e1cb837ae94685c695cfd56a7a1dff3eff046c7e5b105c76b5ed9124e19b9aba1d7f310df0a7e36b8fa3a18b19cb0a08d")]
+[assembly: InternalsVisibleTo("Difi.SikkerDigitalPost.Klient.Tester")]
